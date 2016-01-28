ce2b4faab78776fafec232bb512541cc24e1c698
elijah kim <eli@elijahs-MacBook-Pro-2.local>
2016-01-28 11:09:33 -0500

    Use production config from Heroku ENV variables

--- lib/speak_easy_api/endpoint.ex
+++ lib/speak_easy_api/endpoint.ex
@@ -7,9 +7,6 @@ defmodule SpeakEasyApi.Endpoint do
   #
   # You should set gzip to true if you are running phoenix.digest
   # when deploying your static files in production.
-  plug Plug.Static,
-    at: "/", from: :speak_easy_api, gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
