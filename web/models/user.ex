325941eac44844d6e48966abe34b7f9b391af2b0
Elijah Kim <kimelijah90@gmail.com>
2016-02-02 13:42:04 -0500

    Add permission to user and limit permission input
    * permission defaults to "user" unless set as "admin"

--- web/models/user.ex
+++ web/models/user.ex
@@ -4,18 +4,27 @@ defmodule SpeakEasyApi.User do
   schema "users" do
     field :email, :string
     field :password_hash, :string
+    field :permission, :string
     field :password, :string, virtual: true
 
     timestamps
   end
 
   @required_fields ~w(email password)
-  @optional_fields ~w()
+  @optional_fields ~w(permission)
+  @permission_options ~w(user admin)
 
   def changeset(model, params \\ :empty) do
     model
     |> cast(params, @required_fields, @optional_fields)
     |> put_password_hash
+  end
+
+  def create_user_changeset(model, params \\ :empty) do
+    model
+    |> changeset(params)
+    |> add_permission
+    |> validate_inclusion(:permission, @permission_options)
   end
 
   defp put_password_hash(changeset) do
@@ -24,6 +33,14 @@ defmodule SpeakEasyApi.User do
         put_change(changeset, :password_hash, Comeonin.Bcrypt.hashpwsalt(pass))
       _ ->
         changeset
+    end
+  end
+
+  defp add_permission(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{permission: _}} ->
+        changeset
+      _ -> put_change(changeset, :permission, "user")
     end
   end
 end
